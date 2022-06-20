c4683faa7a09d11db08047bd8cf0640398d05a89
alina-platonova <aliina.platonova@gmail.com>
2022-06-20 22:11:00 +0300

    initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,29 @@
+import classroomSix.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeTest
+    public void pirmsTests() {
+        System.out.println("Sākās tests");
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSum() {
+        System.out.println("Šis ir mans pirmais testing tests");
+        int actualResult = calculator.sum(13,25);
+        int expectedResult = 38;
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testMultiply() {
+        System.out.println("Šis ir mans pirmais testing tests");
+        Assert.assertEquals(calculator.multiply(5,2), 10);
+    }
+}
